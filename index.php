7305d1f1f143c07aa4b67b3dbe54a62a04930f45
Chithambara Kumar <32110271+chithakumar13@users.noreply.github.com>
2020-09-16 08:06:51 +0530

    init commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+    <title>Search</title>
+</head>
+
+<body>
+    <div class="wrapper homepage">
+        <div class="mainsection">
+            <div class="image_container">
+                <img src="assets/images/google_logo.png" />
+            </div>
+            <div class="search_container">
+                <form>
+                    <input type="text" class="search_box" name="searchTerm" />
+                    <input type="submit" class="search_btn" value="Search" />
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
